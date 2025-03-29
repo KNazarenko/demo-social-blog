af10176249a16cbb854e287bca822049699dedba
Nazarenko <nazarenko@MacBook-Pro.local>
2025-03-30 01:05:17 +0200

    refactoring

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,20 +1,11 @@
 import { User } from './definitions';
 import mockUser from './mockData';
 
-export async function fetchUserData() {
+export async function fetchUserData(): Promise<User> {
 	try {
-		// Artificially delay a response for demo purposes.
-		// Don't do this in production :)
-
-		// console.log('Fetching revenue data...');
-		const user: User = await new Promise((resolve) =>
+		return new Promise((resolve) =>
 			setTimeout(() => resolve(mockUser), 3000)
 		);
-		console.log(user);
-
-		// console.log('Data fetch completed after 3 seconds.');
-
-		return user;
 	} catch (error) {
 		console.error('Database Error:', error);
 		throw new Error('Failed to fetch revenue data.');
