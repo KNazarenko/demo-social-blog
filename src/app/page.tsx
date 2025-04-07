fe16d8a5fe50620d440c27c26b350ec3e82cbcab
Nazarenko <nazarenko@MacBook-Pro.local>
2025-04-07 13:58:23 +0300

    add CreatePost component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import CreatePost from '@/components/CreatePost';
+
 export default function Home() {
 	return (
-		<div className="m-4">
-			<h1>Hello home page</h1>
+		<div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+			<div className="lg:col-span-6">
+				<CreatePost />
+			</div>
+
+			<div className="hidden lg:block lg:col-span-4 sticky top-20">
+				WhoToFollow
+			</div>
 		</div>
 	);
 }
