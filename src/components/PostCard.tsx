18ec14e1a7d526845a2292650a2f5aa319f2c328
Nazarenko <nazarenko@MacBook-Pro.local>
2025-04-11 15:18:37 +0300

    add post header and content

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -36,6 +36,36 @@ function PostCard({ post, dbUserId }: { post: Post; dbUserId: string | null }) {
 								/>
 							</Avatar>
 						</Link>
+
+						{/* POST HEADER & TEXT CONTENT */}
+						<div className="flex-1 min-w-0">
+							<div className="flex items-start justify-between">
+								<div className="flex flex-col sm:flex-row sm:items-center sm:space-x-2 truncate">
+									<Link
+										href={`/profile/${post.author.username}`}
+										className="font-semibold truncate">
+										{post.author.name}
+									</Link>
+									<div className="flex items-center space-x-2 text-sm text-muted-foreground">
+										<Link
+											href={`/profile/${post.author.username}`}>
+											@{post.author.username}
+										</Link>
+										<span>•</span>
+										<span>
+											{formatDistanceToNow(
+												new Date(post.createdAt)
+											)}{' '}
+											ago
+										</span>
+									</div>
+								</div>
+								{/* Check if current user is the post author */}
+							</div>
+							<p className="mt-2 text-sm text-foreground break-words">
+								{post.content}
+							</p>
+						</div>
 					</div>
 
 					{/* POST IMAGE */}
