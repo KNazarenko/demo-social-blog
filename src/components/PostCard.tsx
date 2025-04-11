709bc9be7aac0d14b48c3d7ae308c3c71274f109
Nazarenko <nazarenko@MacBook-Pro.local>
2025-04-11 15:46:44 +0300

    create like section

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { getPosts } from '@/actions/post.action';
+import { getPosts, toggleLike } from '@/actions/post.action';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { useState } from 'react';
 import toast from 'react-hot-toast';
@@ -8,6 +8,8 @@ import { Card, CardContent } from './ui/card';
 import Link from 'next/link';
 import { Avatar, AvatarImage } from './ui/avatar';
 import { formatDistanceToNow } from 'date-fns';
+import { Button } from './ui/button';
+import { HeartIcon, MessageCircleIcon } from 'lucide-react';
 
 type Posts = Awaited<ReturnType<typeof getPosts>>;
 type Post = Posts[number];
@@ -23,6 +25,21 @@ function PostCard({ post, dbUserId }: { post: Post; dbUserId: string | null }) {
 	);
 	const [optimisticLikes, setOptmisticLikes] = useState(post._count.likes);
 	const [showComments, setShowComments] = useState(false);
+
+	const handleLike = async () => {
+		if (isLiking) return;
+		try {
+			setIsLiking(true);
+			setHasLiked((prev) => !prev);
+			setOptmisticLikes((prev) => prev + (hasLiked ? -1 : 1));
+			await toggleLike(post.id);
+		} catch (error) {
+			setOptmisticLikes(post._count.likes);
+			setHasLiked(post.likes.some((like) => like.userId === dbUserId));
+		} finally {
+			setIsLiking(false);
+		}
+	};
 
 	return (
 		<Card className="overflow-hidden">
@@ -80,6 +97,51 @@ function PostCard({ post, dbUserId }: { post: Post; dbUserId: string | null }) {
 					)}
 
 					{/* LIKE & COMMENT BUTTONS */}
+					<div className="flex items-center pt-2 space-x-4">
+						{user ? (
+							<Button
+								variant="ghost"
+								size="sm"
+								className={`text-muted-foreground gap-2 ${
+									hasLiked
+										? 'text-red-500 hover:text-red-600'
+										: 'hover:text-red-500'
+								}`}
+								onClick={handleLike}>
+								{hasLiked ? (
+									<HeartIcon className="size-5 fill-current" />
+								) : (
+									<HeartIcon className="size-5" />
+								)}
+								<span>{optimisticLikes}</span>
+							</Button>
+						) : (
+							<a href="/api/auth/login">
+								<Button
+									variant="ghost"
+									size="sm"
+									className="text-muted-foreground gap-2">
+									<HeartIcon className="size-5" />
+									<span>{optimisticLikes}</span>
+								</Button>
+							</a>
+						)}
+
+						<Button
+							variant="ghost"
+							size="sm"
+							className="text-muted-foreground gap-2 hover:text-blue-500"
+							onClick={() => setShowComments((prev) => !prev)}>
+							<MessageCircleIcon
+								className={`size-5 ${
+									showComments
+										? 'fill-blue-500 text-blue-500'
+										: ''
+								}`}
+							/>
+							<span>{post.comments.length}</span>
+						</Button>
+					</div>
 
 					{showComments && (
 						<div className="space-y-4 pt-4 border-t">
