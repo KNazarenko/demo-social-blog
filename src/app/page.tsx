012d5793e96d63d1653a8e1038aa1d41e2bdf59d
Nazarenko <nazarenko@MacBook-Pro.local>
2025-04-11 15:11:14 +0300

    create getPosts action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
+import { getPosts } from '@/actions/post.action';
+import { getAuthUser, getDbUserId } from '@/actions/user.action';
 import CreatePost from '@/components/CreatePost';
+import PostCard from '@/components/PostCard';
 import WhoToFollow from '@/components/WhoToFollow';
+import { IUser } from '@/lib/definitions';
 
-export default function Home() {
+export default async function Home() {
+	const authUser: IUser | null = await getAuthUser();
+
+	const posts = await getPosts();
+	const dbUserId = await getDbUserId();
+
+	console.log(posts);
+
 	return (
 		<div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
 			<div className="lg:col-span-6">
-				<CreatePost />
+				{authUser ? <CreatePost /> : null}
 			</div>
 
 			<div className="hidden lg:block lg:col-span-4 sticky top-20">
