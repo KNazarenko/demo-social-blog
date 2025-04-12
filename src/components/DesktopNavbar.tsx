eb1c555896866e1016902b72641357c06fe85ac2
Nazarenko <nazarenko@MacBook-Pro.local>
2025-04-12 15:01:26 +0300

    remove unused

--- src/components/DesktopNavbar.tsx
+++ src/components/DesktopNavbar.tsx
@@ -32,19 +32,6 @@ async function DesktopNavbar({ user }: { user: IUser | null }) {
 							</span>
 						</Link>
 					</Button>
-					{/* <Button
-						variant="ghost"
-						className="flex items-center gap-2"
-						asChild>
-						<Link
-							href={`/profile/${
-								user?.username ??
-								user?.emailAddresses[0].split('@')[0]
-							}`}>
-							<UserIcon className="w-4 h-4" />
-							<span className="hidden lg:inline">Profile</span>
-						</Link>
-					</Button> */}
 					<Button variant="default">
 						<a href="/api/auth/logout">Logout</a>
 					</Button>
