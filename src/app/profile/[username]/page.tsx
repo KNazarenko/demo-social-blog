bf6f9007e48c7b2ddbf568db2c239f9440d336b6
Nazarenko <nazarenko@MacBook-Pro.local>
2025-04-16 13:25:22 +0300

    implemetn dynamic metadata

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,4 +1,18 @@
-import { getProfileByUsername } from '@/actions/profile.action';
+import { getProfileByUsername, getUserPosts } from '@/actions/profile.action';
+
+export async function generateMetadata({
+	params,
+}: {
+	params: { username: string };
+}) {
+	const user = await getProfileByUsername(params.username);
+	if (!user) return;
+
+	return {
+		title: `${user.name ?? user.username}`,
+		description: user.bio || `Check out ${user.username}'s profile.`,
+	};
+}
 
 async function ProfilePage({ params }: { params: { username: string } }) {
 	console.log('ProfilePage', params);
@@ -7,6 +21,9 @@ async function ProfilePage({ params }: { params: { username: string } }) {
 
 	console.log('ProfilePage', user);
 	if (!user) return;
+
+	const posts = await getUserPosts(user.id);
+	console.log('ProfilePage posts', posts);
 
 	return <div>{user.name ?? user.username} </div>;
 }
