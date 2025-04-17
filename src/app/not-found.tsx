ec417c1b8fb1ab25e26450efe74ff2f594d6ffaa
Nazarenko <nazarenko@MacBook-Pro.local>
2025-04-17 16:37:35 +0300

    add not found for profile page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,20 @@
+import { Button } from '@/components/ui/button';
+import { ArrowLeftIcon } from 'lucide-react';
 import Link from 'next/link';
 
 export default function NotFound() {
 	return (
 		<div>
-			<div className="flex justify-center p-4 border rounded-lg bg-muted/50">
-				<Link href="/">Return Home</Link>
+			<div className="flex justify-center flex-col p-4 border rounded-lg bg-muted/50 ">
+				<p className="text-8xl font-bold text-primary font-mono">404</p>
+				<p className="my-4">Could not find requested resource</p>
+				<Button variant="outline" asChild>
+					<Link href="/">
+						<ArrowLeftIcon className="mr-2 size-4" />
+						Home
+					</Link>
+				</Button>
 			</div>
-			<h2>Not Found</h2>
-			<p>Could not find requested resource</p>
 		</div>
 	);
 }
